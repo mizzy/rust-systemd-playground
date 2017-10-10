63df0f0ca4bb974a60998d6a00b39ffbf04faf6a
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-10 22:36:33 +0900

    Try dbus-rs, but does not work well yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate dbus;
+
+use dbus::{Connection, BusType, Message, MessageItem};
+use dbus::arg;
+use dbus::obj::ObjectPath;
+
 fn main() {
-    println!("Hello, world!");
+    let c = Connection::get_private(BusType::System).unwrap();
+    let m = Message::new_method_call("org.freedesktop.systemd1",
+                                     "/org/freedesktop/systemd1",
+                                     "org.freedesktop.systemd1.Manager",
+                                     "GetUnit")
+        .unwrap()
+        .append1("ssh.service");
+    let r = c.send_with_reply_and_block(m, 2000).unwrap();
+    let o: arg::Path = r.read1().unwrap();
+
+    println!("{:?}", r);
 }
