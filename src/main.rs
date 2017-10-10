dc6619d7f5e9ab6a1bd8adf0f279f773f2010876
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-10 23:07:15 +0900

    Try dbus-bytestream

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-extern crate dbus;
+extern crate dbus_bytestream;
+extern crate dbus_serialize;
+extern crate rustc_serialize;
 
-use dbus::{Connection, BusType, Message, MessageItem};
-use dbus::arg;
-use dbus::obj::ObjectPath;
+use dbus_bytestream::connection::Connection;
+use dbus_bytestream::message;
+use dbus_serialize::decoder::DBusDecoder;
+use rustc_serialize::Decoder;
 
 fn main() {
-    let c = Connection::get_private(BusType::System).unwrap();
-    let m = Message::new_method_call("org.freedesktop.systemd1",
-                                     "/org/freedesktop/systemd1",
-                                     "org.freedesktop.systemd1.Manager",
-                                     "GetUnit")
-        .unwrap()
-        .append1("ssh.service");
-    let r = c.send_with_reply_and_block(m, 2000).unwrap();
-    let o: arg::Path = r.read1().unwrap();
-
-    println!("{:?}", r);
+    let conn = Connection::connect_system().unwrap();
+    let mut msg = message::create_method_call("org.freedesktop.systemd1",
+                                              "/org/freedesktop/systemd1",
+                                              "org.freedesktop.systemd1.Manager",
+                                              "GetUnit");
+    msg = msg.add_arg(&"ssh.service");
+    let reply = conn.call_sync(msg).unwrap().unwrap();
+    let obj = reply[0].clone();
+    println!("{}", DBusDecoder::new(obj).read_str().unwrap());
 }
