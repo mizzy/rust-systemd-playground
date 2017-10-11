ba773c15fef4343a68161edc98439b92dbba6072
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-11 10:17:03 +0900

    Modify dbus-bytestream example, but not works well

--- dbus-bytestream/src/main.rs
+++ dbus-bytestream/src/main.rs
@@ -5,6 +5,7 @@ extern crate rustc_serialize;
 use dbus_bytestream::connection::Connection;
 use dbus_bytestream::message;
 use dbus_serialize::decoder::DBusDecoder;
+use dbus_serialize::types;
 use rustc_serialize::Decoder;
 
 fn main() {
@@ -15,6 +16,23 @@ fn main() {
                                               "GetUnit");
     msg = msg.add_arg(&"ssh.service");
     let reply = conn.call_sync(msg).unwrap().unwrap();
-    let obj = reply[0].clone();
-    println!("{}", DBusDecoder::new(obj).read_str().unwrap());
+    let object_path = DBusDecoder::new(reply[0].clone()).read_str().unwrap();
+
+    println!("{}", object_path);
+
+    let msg = message::create_method_call("org.freedesktop.systemd1",
+                                          &object_path,
+                                          "org.freedesktop.DBus.Properties",
+                                          "Get")
+        .add_arg(&"org.freedesktop.systemd1.Unit")
+        .add_arg(&"ActiveState");
+
+    let reply = conn.call_sync(msg).unwrap().unwrap();
+    let active_state: types::Variant = DBusDecoder::decode(reply[0].clone()).unwrap();
+    println!("{}", active_state);
+
+
 }
+
+
+// [Variant(Variant { object: BasicValue(String("active")), signature: Signature("s") })]
