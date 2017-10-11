77b81e98c25b409f3043db2ea6c10fbfd8f10ec5
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-11 10:40:28 +0900

    Get active state of the service

--- dbus-rs/src/main.rs
+++ dbus-rs/src/main.rs
@@ -1,8 +1,10 @@
 extern crate dbus;
 
 use dbus::{Connection, BusType, Message, Path};
+use dbus::arg;
 
 fn main() {
+    // Get object path by service name
     let c = Connection::get_private(BusType::System).unwrap();
     let m = Message::new_method_call("org.freedesktop.systemd1",
                                      "/org/freedesktop/systemd1",
@@ -12,6 +14,17 @@ fn main() {
         .append1("ssh.service");
     let r = c.send_with_reply_and_block(m, 2000).unwrap();
     let o: Path = r.read1().unwrap();
-
     println!("{}", o);
+
+
+    // Get active state of the service
+    let m = Message::new_method_call("org.freedesktop.systemd1",
+                                     o,
+                                     "org.freedesktop.DBus.Properties",
+                                     "Get")
+        .unwrap()
+        .append2("org.freedesktop.systemd1.Unit", "ActiveState");
+    let r = c.send_with_reply_and_block(m, 2000).unwrap();
+    let s: arg::Variant<&str> = r.read1().unwrap();
+    println!("{}", s.0);
 }
